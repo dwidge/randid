fe569010852c6bd75aee97cba32aacd592d8a9e8
dwidge <dwidge+gh@gmail.com>
2024-09-03 08:16:15 +0200

    Exclude randInt64 from index for old node

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,5 @@
 export * from "./Base32.js";
 export * from "./UnixTimestamp.js";
-export * from "./randInt64.js";
+// export * from "./randInt64.js";
 export * from "./randInt50.js";
 export * from "./randInt.js";
